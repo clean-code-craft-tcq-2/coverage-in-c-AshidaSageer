75b2e07ea08a0f1fdd8fcef16bbacc3db016647c
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-02-23 22:48:04 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -13,22 +13,9 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
+  int lowerLimit = temperaturelimit[coolingType].lowerLimit;
+  int upperLimit = temperaturelimit[coolingType].upperLimit;
+
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
