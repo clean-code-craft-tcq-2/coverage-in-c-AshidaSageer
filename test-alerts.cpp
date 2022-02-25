c8bae3e454a569d5cd5e8925ceba1c0f07cf8a95
AshidaSageer <91050341+AshidaSageer@users.noreply.github.com>
2022-02-25 07:29:59 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -11,4 +11,20 @@ TEST_CASE("infers the breach according to limits") {
   alertTarget = TO_EMAIL;
   REQUIRE(checkAndAlert(alertTarget, batteryChar, 0) == NORMAL);
   REQUIRE(checkAndAlert(alertTarget, batteryChar, 35) == NORMAL);
+  
+  AlertTarget alertTarget = TO_CONTROLLER;
+  BatteryCharacter batteryChar = {HI_ACTIVE_COOLING,"BatteryBrandABC"};
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, -1) == TOO_LOW);
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, 46 ) == TOO_HIGH);
+  alertTarget = TO_EMAIL;
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, 0) == NORMAL);
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, 45) == NORMAL);
+  
+  AlertTarget alertTarget = TO_CONTROLLER;
+  BatteryCharacter batteryChar = {MED_ACTIVE_COOLING,"BatteryBrandABC"};
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, -1) == TOO_LOW);
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, 41 ) == TOO_HIGH);
+  alertTarget = TO_EMAIL;
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, 0) == NORMAL);
+  REQUIRE(checkAndAlert(alertTarget, batteryChar, 40) == NORMAL);
 }
